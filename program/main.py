2a3e1c65141161e7ab53c80848625cd240afd8bb
Mamello Molokwane <161704049+IndigoBlack@users.noreply.github.com>
2026-02-14 19:20:11 +0200

    Updated program/main.py

--- program/main.py
+++ program/main.py
@@ -3,13 +3,18 @@ from google.oauth2 import InstalledAppFlow
 from google.auth.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import questionary
 import datetime
+import csv
 import os
 
 # Medication scheduling. Mabe use google calender for this
 # Track Whether the user has taken their medicine
 
 SCOPE = ["https://www.googleapis.com/auth/calendar"]
+
+def main():
+    ...
 
 def authorise():
     creds = None
@@ -60,6 +65,26 @@ def schedule(service, summary, description, start_date, end_date):
         print("An error occured:", error)
 
 # Track whether the medication has been taken: Self-reporting is a good option.
+def self_track(pills):
+    times = ["00:00", "02:00", "03:00", 
+             "04:00", "05:00", "06:00", 
+             "07:00", "08:00", "09:00", 
+             "10:00", "11:00", 
+             "12:00", "13:00", "14:00", 
+             "15:00", "16:00", "17:00", 
+             "18:00", "19:00", "20:00", 
+             "21:00", "22:00", "23:00"]
+    date = questionary.select("Around which hour did you take your medicine", choices=times)
+    
+    with open("track.csv", "a") as file:
+        writer = csv.writer(file)
+
+        writer.writerow(["Date", "Hour", "How many taken"])
+
+        writer.writerow([datetime.date.today(), date, pills])
+    
+    return "Psuedo-tracked"
+
 # Indicate whether the medication and needs a refill, if it does set a refill date
 # Option:
 #   if possible use google maps API to show pharmacies that are close that the patient can get a refill at
